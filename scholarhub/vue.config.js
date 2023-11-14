ce9119c7e68655fc063edf8f80bc4dc93c13a17b
SAGADESU <1626450436@qq.com>
2023-11-14 17:26:44 +0800

    修改项目基本配置

--- scholarhub/vue.config.js
+++ scholarhub/vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: false,
+  devServer: {
+    open: true,
+    // proxy:{
+    //   '/api':{
+    //     target:``,
+    //     changeOrigin:true,
+    //     pathRewrith:{
+    //       '^/api':''
+    //     }
+    //   }
+    // }
+  }
 })
