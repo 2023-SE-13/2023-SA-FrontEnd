c210cae1f6fa4678c7e43760be3f5b0b0c341149
SAGADESU <1626450436@qq.com>
2023-12-05 16:39:58 +0800

    搜索条样式

--- scholarhub/src/views/HomePage.vue
+++ scholarhub/src/views/HomePage.vue
@@ -2,14 +2,17 @@
     <div class="home-page">
         <div class="img-container">
             <div class="search-bar">
-                <el-select v-model="select" slot="prepend" placeholder="请选择">
-                    <el-option label="餐厅名" value="1"></el-option>
-                    <el-option label="订单号" value="2"></el-option>
-                    <el-option label="用户电话" value="3"></el-option>
-                </el-select>
-                <el-input placeholder="请输入内容" v-model="input3" class="input-with-select">
 
-                    <el-button slot="append" icon="el-icon-search">检索</el-button>
+                <el-input placeholder="发现你感兴趣的内容..." v-model="input3" class="input-with-select">
+                    <el-select class="select" v-model="select" slot="prepend" placeholder="请选择">
+                        <el-option label="标题" value="1"></el-option>
+                        <el-option label="关键词" value="2"></el-option>
+                        <el-option label="作者" value="3"></el-option>
+                    </el-select>
+                    <!-- <el-button slot="append" type="text">文字按钮</el-button> -->
+                    <el-link slot="append" id="pro-search" type="primary" :underline="false">高级检索</el-link>
+                    <!-- <el-button id="pro-search" slot="append" icon="el-icon-search">高级检索</el-button> -->
+                    <el-button slot="append" id="search-button" icon="el-icon-search">检索</el-button>
                 </el-input>
             </div>
         </div>
@@ -76,5 +79,65 @@ export default {
 .input-with-select .el-input-group__prepend {
     background-color: #fff;
 
+}
+
+#pro-search {
+    display: inline-block;
+    margin-right: 40px;
+    background-color: #fff;
+    color: rgba(47, 58, 145, .8) !important;
+    font-size: larger;
+}
+
+#search-button {
+    border-color: #fff;
+    background-color: rgba(47, 58, 145, .8) !important;
+    color: #fff;
+    border-radius: 0;
+    padding: 16px 20px;
+    width: 100px;
+    font-size: larger;
+}
+
+/deep/.el-input-group__append {
+    background-color: #fff;
+    border-color: #fff;
+}
+
+/deep/.el-input__inner {
+    border-color: #fff;
+}
+
+/deep/.el-input-group__prepend {
+    background-color: #fff;
+    border-color: #fff;
+    border-radius: 0;
+}
+
+.select /deep/.el-input__inner::placeholder {
+    color: rgba(47, 58, 145, .8) !important;
+    font-weight: 500;
+    text-align: center;
+}
+
+/deep/.el-select {
+    /* border-right: 1px solid blue; */
+    position: relative;
+
+}
+
+
+/deep/.el-select::after {
+    content: '';
+    position: absolute;
+    top: 25%;
+    /* 调整分隔线位置，这里设置为盒子高度的一半 */
+    right: 0;
+    height: 50%;
+    /* 设置分隔线的高度为盒子高度的一半 */
+    width: 2px;
+    /* 设置分隔线的宽度 */
+    background-color: #d6dbe3;
+    /* 设置分隔线颜色 */
 }
 </style>
--- scholarhub/src/views/Temp.vue
+++ scholarhub/src/views/Temp.vue
@@ -1,16 +1,20 @@
 <template>
     <div>
-        {{temp}}
+        {{ temp }}
+        <el-select id="select-button" v-model="select" slot="prepend" placeholder="请选择">
+            <el-option label="标题" value="1"></el-option>
+            <el-option label="关键词" value="2"></el-option>
+            <el-option label="作者" value="3"></el-option>
+        </el-select>
     </div>
 </template>
 <script>
 export default {
     data() {
         return {
-            temp:"暂定界面1"
+            temp: "暂定界面1"
         }
     }
 }
 </script>
-<style>
-</style>
+<style></style>
