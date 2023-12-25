3315f4f601f211e51f0e673a9a098e789eb8ec74
tadaoni <487811694@qq.com>
2023-12-25 21:53:03 +0800

    token放进localStorage

--- scholarhub/src/store/index.js
+++ scholarhub/src/store/index.js
@@ -5,14 +5,10 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    token:"",
   },
   getters: {
   },
   mutations: {
-    setToken(state,token){
-      state.token = token
-    }
   },
   actions: {
   },
